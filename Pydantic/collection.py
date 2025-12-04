2ecc9b876859a942c5e8ef69fc70e11bb9c88067
acabrelles <aldarcabrelles@gmail.com>
2025-12-04 13:13:37 +0100

    Fixed definitions and pydantic for collection

--- Pydantic/collection.py
+++ Pydantic/collection.py
@@ -22,6 +22,10 @@ class OntologyTerm(BaseModel, extra='forbid'):
             raise ValueError('id must be CURIE, e.g. NCIT:C42331')
         return v
 
+class AgeRange(BaseModel, extra='forbid'):
+    min: float
+    max: float
+
 class Collection(BaseModel, extra='forbid'):
     def __init__(self, **data) -> None:
         for private_key in self.__class__.__private_attributes__.keys():
@@ -35,3 +39,22 @@ class Collection(BaseModel, extra='forbid'):
     name: str
     id: str
     description: Optional[str] = None
+    ageRange:AgeRange
+    modalities: List
+    bodyParts: List
+    gender: List
+    @field_validator('modalities')
+    @classmethod
+    def check_modalities(cls, v):
+        for modality in v:
+            OntologyTerm(**modality)
+    @field_validator('bodyParts')
+    @classmethod
+    def check_bodyParts(cls, v):
+        for body_part in v:
+            OntologyTerm(**body_part)
+    @field_validator('gender')
+    @classmethod
+    def check_gender(cls, v):
+        for gender in v:
+            OntologyTerm(**gender)
