52c34ae6c6ff4eede9367ab7cdbb53cb7ec8458e
acabrelles <aldarcabrelles@gmail.com>
2025-12-04 13:02:43 +0100

    Focused on fixing schema into 3 endpoint format, added collection metadata. Pydantic unfinished

---
+++ Pydantic/collection.py
@@ -0,0 +1,37 @@
+import re
+import argparse
+from dateutil.parser import parse
+from pydantic import (
+    BaseModel,
+    ValidationError,
+    field_validator,
+    PrivateAttr
+)
+
+from typing import Optional, Union, List
+
+class OntologyTerm(BaseModel, extra='forbid'):
+    id: str
+    label: Optional[str]=None
+    @field_validator('id')
+    @classmethod
+    def id_must_be_CURIE(cls, v: str) -> str:
+        if re.match("[A-Za-z0-9]+:[A-Za-z0-9]", v):
+            pass
+        else:
+            raise ValueError('id must be CURIE, e.g. NCIT:C42331')
+        return v
+
+class Collection(BaseModel, extra='forbid'):
+    def __init__(self, **data) -> None:
+        for private_key in self.__class__.__private_attributes__.keys():
+            try:
+                value = data.pop(private_key)
+            except KeyError:
+                pass
+
+        super().__init__(**data)
+    _id: Optional[str] = PrivateAttr()
+    name: str
+    id: str
+    description: Optional[str] = None
